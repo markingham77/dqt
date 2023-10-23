362a5b41bcba3fab4712e33442ae2643d6221d76
Mark Ingham <mark.ingham@ly.st>
2023-10-23 12:10:33 +0100

    __init__

--- pydqt/__init__.py
+++ pydqt/__init__.py
@@ -1,3 +1,25 @@
+from pydqt.pydqt import (
+    set_workdir,
+    workspace,
+    env_reload,
+    env_edit,
+    QueryTemplateParams,
+    QueryParams,
+    Query,
+    Sql
+)
+
+from pydqt import pydqt
+
+
 __all__ = [
-    "pydqt",
+    "set_workdir",
+    "workspace",
+    "env_reload",
+    "env_edit",
+    "QueryTemplateParams",
+    "QueryParams",
+    "Query",
+    "Sql",
+    "pydqt"
 ]
